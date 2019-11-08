3991609e555c10b7bfb773388b2766ded21e2519
olga <olg-199774@mail.ru>
2019-11-08 03:40:33 +0300

    tst

--- src/test/java/ru/bmstu/notebook/api/UserControllerTest.java
+++ src/test/java/ru/bmstu/notebook/api/UserControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,6 +33,7 @@ import java.util.UUID;
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
 @Transactional
+@TestPropertySource("classpath:test.properties")
 public class UserControllerTest {
 
     @Autowired
